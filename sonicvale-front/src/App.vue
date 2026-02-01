e4aca56424d4da494c8e6223efe840967f08fb89
fangwaii <1428390267@qq.com>
2026-02-01 19:32:42 +0800

    build: 更新前端版本号至 1.1.2

--- sonicvale-front/src/App.vue
+++ sonicvale-front/src/App.vue
@@ -50,8 +50,8 @@
 -->
 
       <div class="sider-info" v-if="!collapsed">
-        <div class="info-item">版本：v1.1.0</div>
-        <div class="info-item">配音交流群：575715633（如果群已经满，请按照申请提示进入其他群）</div>
+        <div class="info-item">版本：v1.1.2</div>
+        <div class="info-item">配音交流群：608755030（如果群已经满，请按照申请提示进入其他群）</div>
         <!-- 🔔 醒目声明 -->
         <div class="info-warning">
           
