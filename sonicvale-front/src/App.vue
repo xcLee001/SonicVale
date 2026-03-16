7a59506d309cd343dae69f66848ffb602795a7da
fangwaii <1428390267@qq.com>
2026-03-16 12:09:09 +0800

    docs: 更新配音交流群号
    将群号从608755030更改为1080986764，确保用户能加入正确的交流群组。

--- sonicvale-front/src/App.vue
+++ sonicvale-front/src/App.vue
@@ -51,7 +51,7 @@
 
       <div class="sider-info" v-if="!collapsed">
         <div class="info-item">版本：v1.1.4</div>
-        <div class="info-item">配音交流群：608755030（如果群已经满，请按照申请提示进入其他群）</div>
+        <div class="info-item">配音交流群：1080986764（如果群已经满，请按照申请提示进入其他群）</div>
         <!-- 🔔 醒目声明 -->
         <div class="info-warning">
           
